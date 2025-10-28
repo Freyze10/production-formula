6301d9c87db40eca2144ecd69f986e9f6017f381
Freyze10 <bjabillanoza@gmail.com>
2025-10-28 11:44:44 +0800

    refactored numeric table

--- side_bar/production_manual_entry.py
+++ side_bar/production_manual_entry.py
@@ -11,27 +11,8 @@ import qtawesome as fa
 from db import db_call
 from utils.date import SmartDateEdit
 from utils.work_station import _get_workstation_info
+from utils.numeric_table import NumericTableWidgetItem
 from utils import global_var
-
-
-class NumericTableWidgetItem(QTableWidgetItem):
-    def __init__(self, value, display_text=None, is_float=False):
-        self.value = value
-        self.is_float = is_float
-        if display_text is None:
-            if is_float:
-                display_text = f"{value:.6f}" if value is not None else ""
-            else:
-                display_text = str(value) if value is not None else ""
-        super().__init__(display_text)
-
-    def __lt__(self, other):
-        if isinstance(other, NumericTableWidgetItem):
-            if self.is_float:
-                return float(self.value) < float(other.value)
-            else:
-                return int(self.value) < int(other.value)
-        return super().__lt__(other)
 
 
 class ManualProductionPage(QWidget):
