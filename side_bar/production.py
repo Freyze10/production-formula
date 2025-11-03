772f768eb7fef3408437d6ee7e9771978672f232
Freyze10 <bjabillanoza@gmail.com>
2025-11-03 09:47:42 +0800

    changed view button icon

--- side_bar/production.py
+++ side_bar/production.py
@@ -223,7 +223,7 @@ class ProductionManagementPage(QWidget):
         controls_layout.addWidget(self.view_btn)
 
         self.edit_btn = QPushButton("View - Manual", objectName="InfoButton")
-        self.edit_btn.setIcon(fa.icon('fa5s.edit', color='white'))
+        self.edit_btn.setIcon(fa.icon('fa5.eye', color='white'))
         self.edit_btn.clicked.connect(self.view_manual_prod)
         controls_layout.addWidget(self.edit_btn)
 
