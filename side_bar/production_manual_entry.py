a9616a69e76fc541fa37e53d85952658da02e035
Freyze10 <bjabillanoza@gmail.com>
2025-11-04 15:13:46 +0800

    trying  completer in manual

--- side_bar/production_manual_entry.py
+++ side_bar/production_manual_entry.py
@@ -84,14 +84,14 @@ class ManualProductionPage(QWidget):
         row += 1
 
         # Product Code
-        customer_completer = QCompleter(global_var.production_customer_lists)
-        customer_completer.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
-        customer_completer.setFilterMode(Qt.MatchFlag.MatchStartsWith)
+        product_code_completer = QCompleter(global_var.production_product_code_lists)
+        product_code_completer.setCaseSensitivity(Qt.CaseSensitivity.CaseInsensitive)
+        product_code_completer.setFilterMode(Qt.MatchFlag.MatchStartsWith)
 
         self.product_code_input = QLineEdit()
         self.product_code_input.setPlaceholderText("Enter product code")
         self.product_code_input.setStyleSheet("background-color: #fff9c4;")
-        self.product_code_input.setCompleter(customer_completer)
+        self.product_code_input.setCompleter(product_code_completer)
         primary_layout.addWidget(QLabel("Product Code:"), row, 0)
         primary_layout.addWidget(self.product_code_input, row, 1)
         row += 1
