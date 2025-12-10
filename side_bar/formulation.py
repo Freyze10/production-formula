c94256afa5586f295c595a2d5842d7b9fbbd6f0a
Freyze10 <bjabillanoza@gmail.com>
2025-12-10 11:40:16 +0800

    return back to original

--- side_bar/formulation.py
+++ side_bar/formulation.py
@@ -1318,50 +1318,50 @@ class FormulationManagementPage(QWidget):
         for field in fields:
             field.setEnabled(enable)
 
-    # def add_material_row(self):
-    #     """Add a new material row to the table."""
-    #     material_code = self.material_code_input.currentText()
-    #     if not material_code:
-    #         QMessageBox.warning(self, "Invalid Input", "Please enter a material code.")
-    #         return
-    #
-    #     concentration_text = self.concentration_input.text().strip()
-    #     try:
-    #         concentration = float(concentration_text)
-    #         if concentration <= 0:
-    #             raise ValueError
-    #     except ValueError:
-    #         QMessageBox.warning(self, "Invalid Input", "Please enter a valid concentration.")
-    #         return
-    #
-    #     rm_code = QTableWidgetItem(material_code)
-    #     concentration_value = NumericTableWidgetItem(concentration, is_float=True)
-    #     rm_code.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-    #     concentration_value.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
-    #
-    #     row = self.materials_table.rowCount()
-    #     self.materials_table.insertRow(row)
-    #     self.materials_table.setItem(row, 0, rm_code)
-    #     self.materials_table.setItem(row, 1, concentration_value)
-    #
-    #     self.concentration_input.clear()
-    #     self.material_code_input.lineEdit().setText("")
-    #     self.material_code_input.setFocus()
-    #     self.update_total_concentration()
-    #
-    # def remove_material_row(self):
-    #     """Remove the selected material row."""
-    #     current_row = self.materials_table.currentRow()
-    #     if current_row >= 0:
-    #         self.materials_table.removeRow(current_row)
-    #         self.update_total_concentration()
-    #     else:
-    #         QMessageBox.warning(self, "No Selection", "Please select a row to remove.")
-    #
-    # def clear_materials(self):
-    #     """Clear all material rows."""
-    #     self.materials_table.setRowCount(0)
-    #     self.update_total_concentration()
+    def add_material_row(self):
+        """Add a new material row to the table."""
+        material_code = self.material_code_input.currentText()
+        if not material_code:
+            QMessageBox.warning(self, "Invalid Input", "Please enter a material code.")
+            return
+
+        concentration_text = self.concentration_input.text().strip()
+        try:
+            concentration = float(concentration_text)
+            if concentration <= 0:
+                raise ValueError
+        except ValueError:
+            QMessageBox.warning(self, "Invalid Input", "Please enter a valid concentration.")
+            return
+
+        rm_code = QTableWidgetItem(material_code)
+        concentration_value = NumericTableWidgetItem(concentration, is_float=True)
+        rm_code.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+        concentration_value.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        row = self.materials_table.rowCount()
+        self.materials_table.insertRow(row)
+        self.materials_table.setItem(row, 0, rm_code)
+        self.materials_table.setItem(row, 1, concentration_value)
+
+        self.concentration_input.clear()
+        self.material_code_input.lineEdit().setText("")
+        self.material_code_input.setFocus()
+        self.update_total_concentration()
+
+    def remove_material_row(self):
+        """Remove the selected material row."""
+        current_row = self.materials_table.currentRow()
+        if current_row >= 0:
+            self.materials_table.removeRow(current_row)
+            self.update_total_concentration()
+        else:
+            QMessageBox.warning(self, "No Selection", "Please select a row to remove.")
+
+    def clear_materials(self):
+        """Clear all material rows."""
+        self.materials_table.setRowCount(0)
+        self.update_total_concentration()
 
     def update_total_concentration(self):
         """Update the total concentration display."""
